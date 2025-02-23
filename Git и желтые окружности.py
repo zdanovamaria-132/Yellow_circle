0854e3179b26b82bce5cd99c3abbdcfaba1683ef
Andrey <a.zdanov@gmail.com>
2025-02-23 19:08:55 +0300

    добавлено рисование кругов разных цветов

--- Git и желтые окружности.py
+++ Git и желтые окружности.py
@@ -1,5 +1,5 @@
 import sys
-import random
+from random import randint
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6 import uic
@@ -10,27 +10,30 @@ class CirclePainter(QWidget):
         self.circles = []
 
     def add_circle(self):
-        diameter = random.randint(10, 100)
-        x = random.randint(0, self.width() - diameter)
-        y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+        diameter = randint(10, 100)
+        x = randint(0, self.width() - diameter)
+        y = randint(0, self.height() - diameter)
+        r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+        self.circles.append((x, y, diameter, r, g, b))
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
-        for x, y, diameter in self.circles:
-            painter.setBrush(QColor(255, 255, 0))
+        for x, y, diameter, r, g, b in self.circles:
+            painter.setBrush(QColor(r, g, b))
             painter.drawEllipse(x, y, diameter, diameter)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setWindowTitle("Создание окружностей")
+        self.setGeometry(100, 100, 600, 400)
 
         self.circle_painter = CirclePainter()
         self.setCentralWidget(self.circle_painter)
 
+        self.btn = QPushButton("Добавить окружность", self)
         self.btn.clicked.connect(self.circle_painter.add_circle)
         layout = QVBoxLayout()
         layout.addWidget(self.btn)
